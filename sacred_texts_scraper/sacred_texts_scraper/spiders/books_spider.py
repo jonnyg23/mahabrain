9ecaa6768e93a669f0ad7c75fb6a26d4fd118c17
jonnyg23 <jonguti23@outlook.com>
2023-01-28 00:01:57 -0700

    Add TODO

--- sacred_texts_scraper/sacred_texts_scraper/spiders/books_spider.py
+++ sacred_texts_scraper/sacred_texts_scraper/spiders/books_spider.py
@@ -10,10 +10,26 @@ class BooksSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # TODO: Update with correct values in HTML scrape
-        yield {
-            "title": response.xpath("//h1").get(),
-            "subtitle": response.xpath("//h2").get(),
-            "header": response.xpath("//h3").get(),
-            "body": response.xpath("//p").getall(),
-        }
+        # TODO: Steps for parsing
+        #   1. Only scrape the html body since html is valid markdown
+        #   2. Set rule to only crawl through sacred-texts domain
+        #   3. Research sequence of crawling so that the books are
+        #      added in order (maybe just crawl & gather ALL of the links on
+        #      the domain & sort them before parsing each url)
+        #   4. Init .md file stream with io
+        #   5. Start at index.htm page of the book & continue iterating
+        #      through each of the pages (e.g. /the/iu/iu#.htm) where #
+        #      increases. Add each page/chapter of book to file stream
+        #      - 5.1 Capture book title from index.htm and save as var for
+        #        later use as the book's file name
+        #   6. Continue iteration until 404 Status Code (this is end of book)
+        #   7. Finally save .md file stream to Obsidian Vault
+        #   8. Continue crawling to next book
+
+        item = {}
+        item["url"] = response.url
+        # extract basic body
+        item["body"] = "\n".join(response.xpath("//text()").extract())
+        # or better just save whole source
+        item["source"] = response.body
+        yield item
